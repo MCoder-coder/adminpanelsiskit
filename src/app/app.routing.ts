8f5719c9b66c09a8fa21ca5d0805e807e4aea09c
kalCoder <51723126+MCoder-coder@users.noreply.github.com>
2020-12-04 14:20:59 -0300

    Revert "fakebackend"
    This reverts commit 82c6267148569477ca2cf039a34a29dc18ff292b.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
-import { AuthGuard } from './helpers/auth.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 export const AppRoutes: Routes = [
     {
